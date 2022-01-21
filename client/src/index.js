90e249ca952edf2fb00ae412e0a85344690eb597
Ido Strauss <strauss02@hotmail.co.il>
2022-01-21 17:45:32 +0200

    react router set up

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { render } from 'react-dom'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="teams" element={<Teams />}>
-            <Route path=":teamId" element={<Team />} />
-            <Route path="new" element={<NewTeamForm />} />
-            <Route index element={<LeagueStandings />} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    , document.getElementById("root")
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
