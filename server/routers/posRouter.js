b34f8c01d04015f6bcfd2505017533f8ced30b84
Ido Strauss <strauss02@hotmail.co.il>
2022-01-21 13:16:05 +0200

    all endpoints now functional

--- server/routers/posRouter.js
+++ server/routers/posRouter.js
@@ -15,45 +15,69 @@ const posByNum = {
 }
 
 router.get('/:part', async (req, res) => {
-  console.log(req.query)
-  // if request is sent with query letter then search by letter
+  const pos = posByNum[Number(req.params.part)]
+  req.query.letter = req.query.letter.toUpperCase()
+  const params = {
+    TableName: 'dictionary',
+    IndexName: 'Pos-Word-index',
+    KeyConditionExpression: '#pos = :pos',
+    ExpressionAttributeNames: {
+      '#pos': 'Pos',
+    },
+    ExpressionAttributeValues: {
+      ':pos': pos,
+    },
+    Limit: 10,
+  }
+  // if request is sent with query letter, search by letter
   if (Object.keys(req.query).length === 0 && req.query.constructor === Object) {
-    console.log(req.params.part)
-    const pos = posByNum[Number(req.params.part)]
-
-    params = {
-      TableName: 'dictionary',
-      IndexName: 'Pos-Word-index',
-      KeyConditionExpression: '#pos = :pos',
-      ExpressionAttributeNames: {
-        '#pos': 'Pos',
-      },
-      ExpressionAttributeValues: {
-        ':pos': pos,
-      },
-      Limit: 100,
-    }
-
-    let entries = []
-
     db.query(params, (err, data) => {
       if (err) {
         console.log('error', err)
         res.send('error')
       } else {
         console.log('success')
-        entries = data.Items
-        console.log(entries)
-        return res.send(data.Items)
+        const entries = data.Items
+        const randomEntry = getRandomWord(entries)
+        return res.send(randomEntry)
       }
     })
   } else {
-    res.send('shit')
+    const Bparams = {
+      TableName: 'dictionary',
+      IndexName: 'Pos-Word-index',
+      KeyConditionExpression: '#pos = :pos and begins_with(#word, :letter)',
+      ExpressionAttributeNames: {
+        '#pos': 'Pos',
+        '#word': 'Word',
+      },
+      ExpressionAttributeValues: {
+        ':pos': pos,
+        ':letter': req.query.letter,
+      },
+      Limit: 10,
+    }
+    db.query(Bparams, (err, data) => {
+      if (err) {
+        res.send('shit')
+        console.log(err)
+      } else {
+        const randomEntry = getRandomWord(data.Items)
+        res.send(randomEntry)
+      }
+    })
   }
 })
 
 function getRandomWord(arr) {
-  arr.length
+  const index = getRandomInt(0, arr.length - 1)
+  const randomEntry = arr[index]
+  return randomEntry
 }
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 module.exports = router
