f8d56587bef4b063c82b70314e3ebc69bbb78259
Ido Strauss <strauss02@hotmail.co.il>
2022-01-21 17:24:35 +0200

    create react app
    start developing front

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import { render } from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+function App() {
+  return <div className="App"></div>
+}
+
+export default App
---
+++ client/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { render } from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="teams" element={<Teams />}>
+            <Route path=":teamId" element={<Team />} />
+            <Route path="new" element={<NewTeamForm />} />
+            <Route index element={<LeagueStandings />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    , document.getElementById("root")
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
