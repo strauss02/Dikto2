96e5e15d5c73194e4feeb2cef7f38dd6700fe899
Ido Strauss <strauss02@hotmail.co.il>
2022-01-24 00:35:46 +0200

    develop word views

--- client/src/Components/HomeView.jsx
+++ client/src/Components/HomeView.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Typography, Input } from '@mui/material'
+import { Typography, Input, Button } from '@mui/material'
 import { useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 function HomeView() {
   const [inputValue, setInputValue] = useState('')
   return (
     <div>
-      <Typography> Dikto </Typography>
+      <Typography variant="h2"> Dikto </Typography>
       Welcome my dear friend
       <Input onChange={(e) => setInputValue(e.target.value)}> </Input>
-      <Link to={`/${inputValue}`}> Go to word </Link>
+      <Link to={`/${inputValue}`}>
+        <Button sx={{ display: 'block', mt: 3 }} variant="contained">
+          Go to word
+        </Button>
+      </Link>
     </div>
   )
 }
--- client/src/Components/WordView.jsx
+++ client/src/Components/WordView.jsx
@@ -1,13 +1,29 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-
+import { API_URL } from '../Utils/config'
+import { fetchWordData } from '../Utils/utils'
+import WordCard from './WordCard'
 function WordView(props) {
-  let word = useParams().word
+  let { word } = useParams()
+  const [wordEntries, setWordEntries] = useState([])
 
+  useEffect(() => {
+    async function getEntries() {
+      const entries = await fetchWordData(word)
+      setWordEntries(entries)
+    }
+    getEntries()
+  }, [])
+
+  console.log(wordEntries)
   return (
     <div>
-      <Typography> {word} </Typography>
+      {wordEntries.map((entry) => {
+        return <WordCard entry={entry} />
+      })}
     </div>
   )
 }
